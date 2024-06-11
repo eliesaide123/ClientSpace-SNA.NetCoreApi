b2d825a901468cc95faef8d750334c93acadae0b
Etienne <etienneghanem25@gmail.com>
2024-06-11 08:56:04 +0300

    --mapping from DataSet not DataTable (for more flexibility and microcontrol)

--- ClientSpaceCoreApi/Mappers/ClassMappers.cs
+++ ClientSpaceCoreApi/Mappers/ClassMappers.cs
@@ -8,21 +8,26 @@ namespace ClientSpaceCoreApi.Mappers
     {
         public ClassMappers()
         {
-            CreateMap<DataTable, CredentialsDto>()
-            .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Rows[0]["User_ID"].ToString()))
-            .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Rows[0]["Password"].ToString()))
-            .ForMember(dest => dest.ClientType, opt => opt.MapFrom(src => src.Rows[0]["ClientType"].ToString()))
-            .ForMember(dest => dest.IsAuthenticated, opt => opt.MapFrom(src => src.Rows[0]["IsAuthenticated"].ToString()))
-            .ForMember(dest => dest.IsFirstLogin, opt => opt.MapFrom(src => src.Rows[0]["IsFirstLogin"].ToString()))
-            .ForMember(dest => dest.SessionID, opt => opt.MapFrom(src => src.Rows[0]["SessionID"].ToString()));
+            CreateMap<DataSet, CredentialsDto>()
+            .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Tables["Credentials"].Rows[0]["User_ID"].ToString()))
+            .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Tables["Credentials"].Rows[0]["Password"].ToString()))
+            .ForMember(dest => dest.ClientType, opt => opt.MapFrom(src => src.Tables["Credentials"].Rows[0]["ClientType"].ToString()))
+            .ForMember(dest => dest.IsAuthenticated, opt => opt.MapFrom(src => src.Tables["Credentials"].Rows[0]["IsAuthenticated"].ToString()))
+            .ForMember(dest => dest.IsFirstLogin, opt => opt.MapFrom(src => src.Tables["Credentials"].Rows[0]["IsFirstLogin"].ToString()))
+            .ForMember(dest => dest.SessionID, opt => opt.MapFrom(src => src.Tables["Credentials"].Rows[0]["SessionID"].ToString()));
 
-            CreateMap<DataTable, cUserIdent>()
-            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Rows[0]["FullName"].ToString()))
-            .ForMember(dest => dest.Pin, opt => opt.MapFrom(src => src.Rows[0]["Pin"].ToString()))
-            .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Rows[0]["Role"].ToString()))
-            .ForMember(dest => dest.Language, opt => opt.MapFrom(src => src.Rows[0]["Language"].ToString()))
+            CreateMap<DataSet, cUserIdent>()
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Tables["UserIdent"].Rows[0]["FullName"].ToString()))
+            .ForMember(dest => dest.Pin, opt => opt.MapFrom(src => src.Tables["UserIdent"].Rows[0]["Pin"].ToString()))
+            .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Tables["UserIdent"].Rows[0]["Role"].ToString()))
+            .ForMember(dest => dest.Language, opt => opt.MapFrom(src => src.Tables["UserIdent"].Rows[0]["Language"].ToString()))
+            .ForMember(dest => dest.RoleID, opt => opt.MapFrom(src => GetRoleID(src.Tables["Codes"].Rows[0]["Code"].ToString())))
             .ForMember(dest => dest.LoggedDate, opt => opt.MapFrom(src => DateTime.Now.ToShortDateString()));
 
+        }
+        private string GetRoleID(string code)
+        {
+            return code.Split("-")[0];
         }
     }
 }
